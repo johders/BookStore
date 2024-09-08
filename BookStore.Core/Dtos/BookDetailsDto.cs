c6126a625274f95a667f8ea3506699b0d6bfcc38
jd-HW <johannes.dereuddre@student.howest.be>
2024-09-08 12:04:23 +0200

    Add book detail page

--- BookStore.Core/Dtos/BookDetailsDto.cs
+++ BookStore.Core/Dtos/BookDetailsDto.cs
@@ -1,6 +1,12 @@
 ﻿namespace BookStore.Core.Dtos
 {
 	public record BookDetailsDto(int Id, string Title, string Image, AuthorDto Author, int NumPages, string Format,
-		string Description, GenreDto[] Genres, string? BuyLink);
+		string Description, GenreDto[] Genres, string? BuyLink)
+	{
+		public string BookLink => string.IsNullOrWhiteSpace(BuyLink) 
+			? $"https://www.google.com/search?q={Title.Replace(" ", "+")}+by+{Author.Name.Replace(" ", "+")}" : BuyLink;
+
+	}
+
 
 }
